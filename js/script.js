d2596803f9e6fa4b06527dd070ff88411a2830b0
pedrohmcruz <devpedromoreiracruz@gmail.com>
2023-02-23 10:29:52 -0300

    first

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const newTask = document.querySelector('#new-task');
+const tasksList = document.querySelector('#tasks');
+const btn = document.querySelector('#add-task');
+const modal = document.querySelector('#modal');
+
+btn.addEventListener('click', () => {
+  const newTaskValue = newTask.value;
+  
+  if(newTaskValue == "") {
+    modal.style.display = 'flex';
+  }
+
+  const p = document.createElement('p');
+  const li = document.createElement('li');
+  li.appendChild(p);
+
+  p.textContent = newTaskValue;
+
+  const btnRemove = document.createElement('button');
+  btnRemove.className = 'btn-remove'
+  btnRemove.textContent = 'X';
+
+  btnRemove.addEventListener('click', () => {
+    li.remove();
+  });
+
+  li.appendChild(btnRemove);
+  tasksList.appendChild(li);
+
+  newTask.value = '';
+});
